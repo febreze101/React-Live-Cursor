3499a70db115730ce96f9039f2724a26b28ea6b0
Fabrice Bokovi <Fabrice.Bokovi@virtualincision.com>
2024-05-23 20:49:38 -0700

    init commit

---
+++ client/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import './App.css'
+import {Login} from "./components/login"
+import { Home } from './components/Home'
+
+function App() {
+
+  const [username, setUsername] = useState("")
+
+  return username ? (
+    <Home username={username}/>
+  ) : (
+    < Login onSubmit={setUsername}/>
+  )
+
+  // return (
+  //   <>
+  //     <Login onSubmit={setUsername} />
+  //   </>
+  // )
+}
+
+export default App
